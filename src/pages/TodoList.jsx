ecea2be8a90449c41a3b4d7f441b865df04bf8ff
Calvin Putera Loka <50194060+Calvphl@users.noreply.github.com>
2021-06-01 02:41:16 +0700

    merefactor todos dan todolist component

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Paper from "../components/Paper";
 import Header from "../components/Header";
@@ -6,11 +6,17 @@ import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { text: "makan pagi" },
+    { text: "makan siang" },
+    { text: "makan malam" }
+  ]);
+
   return (
     <Paper>
       <Header />
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </Paper>
   );
 };
